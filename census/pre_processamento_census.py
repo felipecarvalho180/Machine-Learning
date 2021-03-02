dcfff2f50ab18d6b5d820702ff2a34796bdb722f
Felipe <felipecarvalho180@gmail.com>
2021-03-01 22:48:14 -0300

    Finalizado pre processamento usando a base census

--- census/pre_processamento_census.py
+++ census/pre_processamento_census.py
@@ -5,3 +5,26 @@ Created on Mon Mar  1 21:51:07 2021
 @author: Felipe
 """
 
+import pandas as pd
+base = pd.read_csv('census.csv')
+previsores = base.iloc[:, 0:14].values
+classes = base.iloc[:, 14].values
+
+#TRANFORMANDO STRING EM NUMERICO
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_previsores= LabelEncoder()
+previsores[:, 1] = labelEncoder_previsores.fit_transform(previsores[:, 1])
+previsores[:, 3] = labelEncoder_previsores.fit_transform(previsores[:, 3])
+previsores[:, 5] = labelEncoder_previsores.fit_transform(previsores[:, 5])
+previsores[:, 6] = labelEncoder_previsores.fit_transform(previsores[:, 6])
+previsores[:, 7] = labelEncoder_previsores.fit_transform(previsores[:, 7])
+previsores[:, 8] = labelEncoder_previsores.fit_transform(previsores[:, 8])
+previsores[:, 9] = labelEncoder_previsores.fit_transform(previsores[:, 9])
+previsores[:, 13] = labelEncoder_previsores.fit_transform(previsores[:, 13])
+
+# TRANFORMANDO CATEGORIA ORDINAL EM NOMINAL
+
+from sklearn.compose import ColumnTransformer
+oneHotEncoder = ColumnTransformer(transformers=[('OneHot', OneHotEncoder(), [1,3,5,6,7,8,9,13])], remainder='passthrough')
+previsores = oneHotEncoder.fit_transform(previsores).toarray()
