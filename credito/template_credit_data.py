be6fea9bf48562e58774a43d4def5937a668814b
felipecarvalho180 <felipecarvalho180@gmail.com>
2021-03-16 00:00:59 -0300

    Finalizado scikit-learn

--- credito/template_credit_data.py
+++ credito/template_credit_data.py
@@ -18,3 +18,12 @@ previsores = scaler.fit_transform(previsores)
 
 from sklearn.model_selection import train_test_split
 previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25, random_state=0)
+
+from sklearn.tree import DecisionTreeClassifier
+classificador = DecisionTreeClassifier(criterion='entropy', random_state=0)
+classificador.fit(previsores_treinamento, classe_treinamento)
+previsores = classificador.predict(previsores_teste)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+precisao = accuracy_score(classe_teste, previsores)
+matriz = confusion_matrix(classe_teste, previsores)
